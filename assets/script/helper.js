15206e27797ed0a3cc41bcac3b471ad20a05a56d
jaspreet <jasspreet4640@gmail.com>
2026-03-26 01:45:46 -0500

    add helper function

---
+++ assets/script/helper.js
@@ -0,0 +1,34 @@
+export function getDateText() {
+    const today = new Date();
+    return today.toLocaleString();
+}
+
+export function createPostElement(user, text, imageSource) {
+    const post = document.createElement('article');
+    post.className = 'post-card';
+
+    let textHtml = '';
+    let imageHtml = '';
+
+    if (text !== '') {
+        textHtml = `<p class="post-text">${text}</p>`;
+    }
+
+    if (imageSource) {
+        imageHtml = `<img src="${imageSource}" alt="Post image" class="post-image">`;
+    }
+
+    post.innerHTML = `
+        <div class="post-header">
+            <img src="./assets/image/profile.jpg" alt="Profile picture" class="profile-pic">
+            <div>
+                <p class="post-name">${user.name}</p>
+                <p class="post-date">${getDateText()}</p>
+            </div>
+        </div>
+        ${textHtml}
+        ${imageHtml}
+    `;
+
+    return post;
+}
